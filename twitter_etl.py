fa7d7d8874eab0b9d54124690b174545ebf6d128
Darshil Parmar <22773771+darshilparmar@users.noreply.github.com>
2022-09-20 16:38:24 +0530

    Create twitter_etl.py

---
+++ twitter_etl.py
@@ -0,0 +1,43 @@
+import tweepy
+import pandas as pd 
+import json
+from datetime import datetime
+import s3fs 
+
+def run_twitter_etl():
+
+    access_key = "" 
+    access_secret = "" 
+    consumer_key = ""
+    consumer_secret = ""
+
+
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)   
+    auth.set_access_token(consumer_key, consumer_secret) 
+
+    # # # Creating an API object 
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(screen_name='@elonmusk', 
+                            # 200 is the maximum allowed count
+                            count=200,
+                            include_rts = False,
+                            # Necessary to keep full_text 
+                            # otherwise only the first 140 words are extracted
+                            tweet_mode = 'extended'
+                            )
+
+    list = []
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {"user": tweet.user.screen_name,
+                        'text' : text,
+                        'favorite_count' : tweet.favorite_count,
+                        'retweet_count' : tweet.retweet_count,
+                        'created_at' : tweet.created_at}
+        
+        list.append(refined_tweet)
+
+    df = pd.DataFrame(list)
+    df.to_csv('refined_tweets.csv')
