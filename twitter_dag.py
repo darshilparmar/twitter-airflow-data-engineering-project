59831cbf28f329acd9ce186c0e7c5a9a4ea56fbb
Darshil Parmar <22773771+darshilparmar@users.noreply.github.com>
2022-09-20 16:37:16 +0530

    Create twitter_dag.py

---
+++ twitter_dag.py
@@ -0,0 +1,38 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+from twitter_etl import run_twitter_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'twitter_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1),
+)
+
+dag = DAG(
+    'twitter_dag',
+    default_args=default_args,
+    description='My first etl code'
+)
+
+run_etl = PythonOperator(
+    task_id='complete_twitter_etl',
+    python_callable=run_twitter_etl,
+    dag=dag, 
+)
+
+run_etl
